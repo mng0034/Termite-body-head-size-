16591e404c5e6e667e48da2d8305a2992a6287dd
mng0034 <mng0034@auburn.edu>
2026-04-09 13:17:59 -0500

    Refactor analysis script for clarity and structure

--- analysis.R
+++ analysis.R
@@ -1,135 +1,188 @@
-# dependencies
-library(dplyr)
-library(tidyr)
-library(ggplot2)
+# [Termite body and head analysis]
+# Title: Determining differences in body size variability between pseudergate and true worker species in termites
+# Author: [Mary Goldman] 
+# Date: [2026-09-04]
+
+# Install and import packages 
+
+{
+  library(dplyr)
+  library(tidyr)
+  library(ggplot2)
+}
 
 # load dataset
-dm = read.csv("data_fmt/Dataset_R.csv")
 
-#standard deviation
-result <- dm %>%
-  group_by(Species) %>%
-  summarise(
-    sd_f_head = sd(f_head, na.rm = TRUE),
-    sd_m_head = sd(m_head, na.rm = TRUE),
-    sd_f_body = sd(f_body, na.rm = TRUE),
-    sd_m_body = sd(m_body, na.rm = TRUE)
-  )
-# view by SD By species
-print(result)
+{
+  dm = read.csv("data_fmt/Dataset_R.csv")
+}
+
+# Find standard deviation
+{
+    result <- dm %>%
+      group_by(Species) %>%
+      summarise(
+        sd_f_head = sd(f_head, na.rm = TRUE),
+        sd_m_head = sd(m_head, na.rm = TRUE),
+        sd_f_body = sd(f_body, na.rm = TRUE),
+        sd_m_body = sd(m_body, na.rm = TRUE)
+       )
+}
+
+# View by SD By species
+
+{
+  print(result)
+}
 
 #Prepare variation table
-result <- dm %>%
-  group_by(Species, Development) %>%
-  summarise(
-    sd_f_head = sd(f_head, na.rm = TRUE),
-    sd_m_head = sd(m_head, na.rm = TRUE),
-    sd_f_body = sd(f_body, na.rm = TRUE),
-    sd_m_body = sd(m_body, na.rm = TRUE),
-    .groups = "drop"
-  )
-result_long <- result %>%
-  pivot_longer(
-    cols = c(sd_f_head, sd_m_head, sd_f_body, sd_m_body),
-    names_to = "measure",
-    values_to = "variation"
-  ) %>%
-  separate(measure, into = c("stat", "sex", "trait"), sep = "_") %>%
-  select(-stat)
-names(result_long)
 
-result_dev <- result_long %>%
-  group_by(Development, sex, trait) %>%
-  summarise(
-    mean_var = mean(variation, na.rm = TRUE),
-    se = sd(variation, na.rm = TRUE) / sqrt(n()),
-    .groups = "drop"
-  )
+{
+    result <- dm %>%
+       group_by(Species, Development) %>%
+       summarise(
+          sd_f_head = sd(f_head, na.rm = TRUE),
+          sd_m_head = sd(m_head, na.rm = TRUE),
+          sd_f_body = sd(f_body, na.rm = TRUE),
+          sd_m_body = sd(m_body, na.rm = TRUE),
+          .groups = "drop"
+         )
+}
+
+{
+    result_long <- result %>%
+      pivot_longer(
+        cols = c(sd_f_head, sd_m_head, sd_f_body, sd_m_body),
+        names_to = "measure",
+        values_to = "variation"
+       ) %>%
+        separate(measure, into = c("stat", "sex", "trait"), sep = "_") %>%
+        select(-stat)
+        names(result_long)
+}
+
+{
+    result_dev <- result_long %>%
+      group_by(Development, sex, trait) %>%
+      summarise(
+        mean_var = mean(variation, na.rm = TRUE),
+        se = sd(variation, na.rm = TRUE) / sqrt(n()),
+        .groups = "drop"
+      )
+}
+
 #Plot Developmental Variation between females and males
-ggplot(result_dev, aes(x = Development, y = mean_var, fill = sex)) +
-  geom_col(position = "dodge") +
-  geom_errorbar(
-    aes(ymin = mean_var - se, ymax = mean_var + se),
-    width = 0.2,
-    position = position_dodge(0.9)
-  ) +
-  facet_wrap(~trait) +
-  theme_classic() +
-  labs(
-    x = "Development",
-    y = "Mean variation",
-    fill = "Sex"
-  )
+
+{
+     ggplot(result_dev, aes(x = Development, y = mean_var, fill = sex)) +
+        geom_col(position = "dodge") +
+        geom_errorbar(
+          aes(ymin = mean_var - se, ymax = mean_var + se),
+          width = 0.2,
+          position = position_dodge(0.9)
+        ) +
+        facet_wrap(~trait) +
+        theme_classic() +
+        labs(
+          x = "Development",
+          y = "Mean variation",
+          fill = "Sex"
+        )
+}
 
 #Prepare variation table
-result_overall <- dm %>%
-  group_by(Species, Development) %>%
-  summarise(
-    head_var = mean(c(
-      sd(f_head, na.rm = TRUE),
-      sd(m_head, na.rm = TRUE)
-    ), na.rm = TRUE),
-    body_var = mean(c(
-      sd(f_body, na.rm = TRUE),
-      sd(m_body, na.rm = TRUE)
-    ), na.rm = TRUE),
-    .groups = "drop"
-  )
-result_overall_long <- result_overall %>%
-  pivot_longer(
-    cols = c(head_var, body_var),
-    names_to = "trait",
-    values_to = "variation"
-  )
-# Plot Overall Variation
-result_dev <- result_overall_long %>%
-  group_by(Development, trait) %>%
-  summarise(
-    mean_var = mean(variation, na.rm = TRUE),
-    se = sd(variation, na.rm = TRUE) / sqrt(n()),
-    .groups = "drop"
-  )
 
-ggplot(result_dev, aes(x = Development, y = mean_var, fill = Development)) +
-  geom_col() +
-  geom_errorbar(
-    aes(ymin = mean_var - se, ymax = mean_var + se),
-    width = 0.2
-  ) +
-  facet_wrap(~trait) +
-  theme_classic() +
-  labs(
-    x = "Development",
-    y = "Mean variation",
-    title = "Overall variation by developmental type"
+{
+    result_overall <- dm %>%
+      group_by(Species, Development) %>%
+      summarise(
+          head_var = mean(c(
+            sd(f_head, na.rm = TRUE),
+            sd(m_head, na.rm = TRUE)
+          ), na.rm = TRUE),
+          body_var = mean(c(
+            sd(f_body, na.rm = TRUE),
+            sd(m_body, na.rm = TRUE)
+          ), na.rm = TRUE),
+          .groups = "drop"
+        )
+}
+
+{
+      result_overall_long <- result_overall %>%
+          pivot_longer(
+            cols = c(head_var, body_var),
+            names_to = "trait",
+            values_to = "variation"
+          )
+}
+
+# Plot Overall Variation
+
+{
+      result_dev <- result_overall_long %>%
+        group_by(Development, trait) %>%
+        summarise(
+            mean_var = mean(variation, na.rm = TRUE),
+            se = sd(variation, na.rm = TRUE) / sqrt(n()),
+            .groups = "drop"
+          )
+}
+
+{
+        ggplot(result_dev, aes(x = Development, y = mean_var, fill = Development)) +
+          geom_col() +
+          geom_errorbar(
+            aes(ymin = mean_var - se, ymax = mean_var + se),
+            width = 0.2
+           ) +
+           facet_wrap(~trait) +
+           theme_classic() +
+           labs(
+             x = "Development",
+             y = "Mean variation",
+             title = "Overall variation by developmental type"
+            )
+  }
+
+{
+        ggplot(result_dev, aes(x = Development, y = mean_var, fill = Development)) +
+          geom_col(width = 0.7, color = "black") +
+          geom_errorbar(
+              aes(ymin = mean_var - se, ymax = mean_var + se),
+              width = 0.15
+            ) +
+            facet_wrap(~trait, scales = "free_y") +
+            theme_classic(base_size = 14) +
+            scale_fill_manual(values = c(
+                "Pseudergate" = "#0072B2",  # blue
+                "TW" = "#E69F00"        # orange
+              ))#+
+              theme(
+                legend.position = "none",
+                axis.text.x = element_text(angle = 30, hjust = 1),
+                strip.text = element_blank(),
+                strip.background = element_blank())+
+              labs(
+                x = "Worker Type",
+                y = "Mean variation",
+            )
+  }
+
+# Prepare variation of size graph
+
+{
+      dm_long <- dm %>%
+        pivot_longer(
+          cols = c(f_head, f_body, m_head, m_body),
+          names_to = c("sex", ".value"),
+          names_sep = "_"
   )
-ggplot(result_dev, aes(x = Development, y = mean_var, fill = Development)) +
-  geom_col(width = 0.7, color = "black") +
-  geom_errorbar(
-    aes(ymin = mean_var - se, ymax = mean_var + se),
-    width = 0.15
-  ) +
-  facet_wrap(~trait, scales = "free_y") +
-  theme_classic(base_size = 14) +
-  scale_fill_manual(values = c(
-    "Pseudergate" = "#0072B2",  # blue
-    "TW" = "#E69F00"        # orange
-  ))#+
-  theme(
-    legend.position = "none",
-    axis.text.x = element_text(angle = 30, hjust = 1),
-    strip.text = element_blank(),
-    strip.background = element_blank())+
-  labs(
-    x = "Worker Type",
-    y = "Mean variation",
-  )
-# Prepare and plot variation of size graph
-dm_long <- dm %>%
-  pivot_longer(
-    cols = c(f_head, f_body, m_head, m_body),
-    names_to = c("sex", ".value"),
-    names_sep = "_"
-  )
-ggplot(dm_long, aes(x = head, y= body, col = sex )) +
-  geom_point()
+}
+
+# Plot variation of size graph
+
+{
+    ggplot(dm_long, aes(x = head, y= body, col = sex )) +
+        geom_point()
+}
